e3dde2195f4ebd367037b68cca29fbd6e3b50a80
Pravin Chaudhari <[prvinchaudhari@gmail.com]>
2026-01-30 14:45:45 +0530

    Adding UC2 use case files

--- QuantityMeasureApplication/src/main/java/QuantityMeasureApp.java
+++ QuantityMeasureApplication/src/main/java/QuantityMeasureApp.java
@@ -11,39 +11,103 @@ public class QuantityMeasureApp {
             this.value1 = value1;
         }
 
+        public double toInches(){
+            return value1 * 12;
+        }
+
         @Override
         public boolean equals(Object obj) {
 
             //Reference
             if (this == obj) {
-                System.out.println("Checking Reference.....");
+                System.out.println("Checking Both Feet class Reference:- "+this+" , " + obj);
                 return true;
             }
 
-            if (obj == null || getClass() != obj.getClass() ) {
-                System.out.println("Checking Null .....");
+            if (obj == null) {
+                System.out.println("Checking feet class object  Null:- "+this+" , " + obj);
+                return false;
+            }
+
+            if ( getClass() != obj.getClass() ) {
+                System.out.println("Checking Class Type:- "+this+" , " + obj.getClass());
                 return false;
             }
 
             Feet feet = (Feet) obj;
-            System.out.println("Checking double Compare method.....");
+            System.out.println("Checking feet class Double Compare Method.:- "+feet.value1 +" , " + this.value1);
             return Double.compare(feet.value1, this.value1) == 0;
         }
+
+
+    }
+    public static class Inches {
+
+        private final double value1;
+
+        public Inches(double value1) {
+            this.value1 = value1;
+        }
+
+
+
+        @Override
+        public boolean equals(Object obj) {
+
+            //Reference
+            if (this == obj) {
+                System.out.println("Checking Inches Class Reference:- "+this+" , " + obj);
+                return true;
+            }
+
+            if (obj == null ) {
+                System.out.println("Checking Inches class Null Comparison:- "+this+" , " + obj);
+                return false;
+            }
+            if (obj instanceof Feet)
+            {
+                Feet feet = (Feet) obj;
+                System.out.println("Checking feet class Double Compare Method.:- "+feet.value1 +" , " + this.value1);
+                return Double.compare(this.value1, feet.toInches()) == 0;
+            }
+            if(obj instanceof Inches) {
+                Inches inches = (Inches) obj;
+                System.out.println("Checking Inches double Compare method:- " + inches.value1 + " , " + this.value1);
+                return Double.compare(inches.value1, this.value1) == 0;
+            }
+            if ( getClass() != obj.getClass() ) {
+                System.out.println("Checking Class type:- "+this+" , " + obj.getClass());
+                return false;
+            }
+            return false;
+
+        }
+
+    }
+    public static void demonstrateFeetEquality(double number1,double number2){
+        QuantityMeasureApp.Feet obj1 = new QuantityMeasureApp.Feet(number1);
+        QuantityMeasureApp.Feet obj2 = new QuantityMeasureApp.Feet(number2);
+        QuantityMeasureApp.Feet obj3=null;
+        System.out.println("Feet Both Object Value is :- "+(obj1.equals(obj2)));
+        System.out.println("Feet Both Object Value is :- "+(obj1.equals(obj3)));
+    }
+
+    public static void demonstrateInchesEquality(double number1,double number2){
+        QuantityMeasureApp.Inches obj1 = new QuantityMeasureApp.Inches(number1);
+        QuantityMeasureApp.Inches obj2 = new QuantityMeasureApp.Inches(number2);
+        QuantityMeasureApp.Inches obj3=null;
+        System.out.println("Inches Both Object Value is :- "+(obj1.equals(obj2)));
+        System.out.println("Inches Both Object Value is :- "+(obj1.equals(obj3)));
+
+        QuantityMeasureApp.Inches inches = new QuantityMeasureApp.Inches(number1);
+        QuantityMeasureApp.Feet feet = new QuantityMeasureApp.Feet(number2);
+        System.out.println(inches.equals(feet)); // True
+       // System.out.println(inches.equals(feet)); // false
     }
 
     public static void main(String[] args) {
-
-        QuantityMeasureApp.Feet obj4 = null;
-        QuantityMeasureApp.Feet obj1 = new QuantityMeasureApp.Feet(100);
-        QuantityMeasureApp.Feet obj2 = new QuantityMeasureApp.Feet(100);
-        QuantityMeasureApp.Feet obj3=null;
-
-
-        System.out.println("Both Object Value is :- "+(obj1.equals(obj2)));
-        System.out.println("Both Object Value is :- "+(obj1.equals(obj3)));
-        //System.out.println("Both Object Value is :- "+(Objects.equals(obj4,obj3)));
-        System.out.println("Both Object Value is :- "+(obj3.equals(obj1)));
-
-
+        demonstrateFeetEquality(1.0,1.0);
+        demonstrateInchesEquality(12,1.0);
+       // demonstrateInchesEquality(24,1.0);
     }
 }
--- QuantityMeasureApplication/src/test/java/QuantityMeasureAppTest.java
+++ QuantityMeasureApplication/src/test/java/QuantityMeasureAppTest.java
@@ -8,47 +8,88 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class QuantityMeasureAppTest {
 
         private QuantityMeasureApp.Feet feet;
+        private QuantityMeasureApp.Inches inches;
 
         @BeforeEach
         void setUp() {
-            feet = new QuantityMeasureApp.Feet(100);
+            feet = new QuantityMeasureApp.Feet(1.0);
+            inches=new QuantityMeasureApp.Inches(1.0);
         }
 
         @Test
         void testFeetEquality_sameValue() {
-            QuantityMeasureApp.Feet obj1 = new QuantityMeasureApp.Feet(100);
-            QuantityMeasureApp.Feet obj2 = new QuantityMeasureApp.Feet(100);
-            assertEquals(true,obj1.equals(obj2));
+            QuantityMeasureApp.Feet obj2 = new QuantityMeasureApp.Feet(1.0);
+            assertEquals(true,feet.equals(obj2));
         }
 
     @Test
     void testFeetEquality_differentValue() {
-        QuantityMeasureApp.Feet obj1 = new QuantityMeasureApp.Feet(100);
-        QuantityMeasureApp.Feet obj2 = new QuantityMeasureApp.Feet(200);
-        assertEquals(false,obj1.equals(obj2));
+        QuantityMeasureApp.Feet obj2 = new QuantityMeasureApp.Feet(2.0);
+        assertEquals(false,feet.equals(obj2));
     }
 
     @Test
     void testFeetEquality_nullComparison() {
-      //  QuantityMeasureApp.Feet obj1 = new QuantityMeasureApp.Feet(100);
-        QuantityMeasureApp.Feet obj1 = null;
-        QuantityMeasureApp.Feet obj2 = new QuantityMeasureApp.Feet(100);
-        assertEquals(false, obj1.equals(obj2));
+        QuantityMeasureApp.Feet obj2 = null;
+        assertEquals(false, feet.equals(obj2));
        // assertEquals(false, Objects.equals(obj1,obj2));
     }
 
     @Test
     void testFeetEquality_differentClass() {
-        QuantityMeasureApp.Feet obj1 = new QuantityMeasureApp.Feet(100);
         Object obj2 = new String();
-        assertEquals(false,obj1.equals(obj2));
+        assertEquals(false,feet.equals(obj2));
     }
 
     @Test
     void testFeetEquality_sameReference() {
-        QuantityMeasureApp.Feet obj1 = new QuantityMeasureApp.Feet(100);
-        QuantityMeasureApp.Feet obj2 = obj1;
-        assertEquals(true,obj1.equals(obj2));
+        QuantityMeasureApp.Feet obj2 = feet;
+        assertEquals(true,feet.equals(obj2));
+    }
+
+    @Test
+    void testInchesEquality_sameValue() {
+        QuantityMeasureApp.Inches obj2 = new QuantityMeasureApp.Inches(1.0);
+        assertEquals(true,inches.equals(obj2));
+    }
+
+    @Test
+    void testInchesEquality_differentValue() {
+        QuantityMeasureApp.Inches obj2 = new QuantityMeasureApp.Inches(2.0);
+        assertEquals(false,inches.equals(obj2));
+    }
+
+    @Test
+    void testInchesEquality_nullComparison() {
+        QuantityMeasureApp.Inches obj2 = null;
+        assertEquals(false, inches.equals(obj2));
+        // assertEquals(false, Objects.equals(obj1,obj2));
+    }
+
+    @Test
+    void testInchesEquality_differentClass() {
+        Object obj2 = new String();
+        assertEquals(false,inches.equals(obj2));
+    }
+
+    @Test
+    void testInchesEquality_sameReference() {
+        QuantityMeasureApp.Inches obj2 = inches;
+        assertEquals(true,inches.equals(obj2));
+    }
+
+    @Test
+    void testEquality_bothFeetAndInchesEquality() {
+        QuantityMeasureApp.Inches inches = new QuantityMeasureApp.Inches(12);
+        QuantityMeasureApp.Feet feet = new QuantityMeasureApp.Feet(1);
+        assertEquals(true,inches.equals(feet));
+    }
+
+    @Test
+    void testNotEquality_bothFeetAndInchesNotEquality() {
+        QuantityMeasureApp.Inches inches = new QuantityMeasureApp.Inches(14);
+        QuantityMeasureApp.Feet feet = new QuantityMeasureApp.Feet(1);
+        assertEquals(false,inches.equals(feet));
     }
 
 }
